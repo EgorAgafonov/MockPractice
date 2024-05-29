1e48f1d2095e0dbcd818bbca8cc7452c09cf656a
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-05-29 17:34:01 +0300

    Updated files

--- test/MockSimpleRequest.py
+++ test/MockSimpleRequest.py
@@ -6,13 +6,29 @@ import pytest
 
 class TestMockAPI:
 
-    def test_mock_simple_request(self):
+    def test_mock_request_apikey(self):
         """"""
+
         headers = {"api_key": "qwerty"}
         request = requests.get(mock_url + '/api/key', headers=headers)
         result = request.json()
         status = request.status_code
-        print(status)
-        print(type(result))
+        print(f"\n{result}")
+
+        assert status == 200
+        assert len(result["key"]) >= 7
+
+    def test_mock_request_userid(self):
+        """"""
+
+        headers = {"api_key": "qwerty"}
+        request = requests.get(mock_url + '/users', headers=headers)
+        result = request.json()
+        status = request.status_code
+        print(f"\n{result}")
+
+        assert status == 200
+        assert str(result["user_id"]).isdigit
+
 
 
