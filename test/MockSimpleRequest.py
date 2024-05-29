01f52f7c008c1e448a6d935bc02be842bc4aa931
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-05-29 11:44:25 +0300

    Updated files

--- test/MockSimpleRequest.py
+++ test/MockSimpleRequest.py
@@ -8,7 +8,7 @@ class TestMockAPI:
     def test_mock_simple_request(self):
         """"""
 
-        request = requests.get("https://e2a64595-8bb4-4475-9cf3-cc189ea65fbc.mock.pstmn.io/user_id")
+        request = requests.get("")
         result = request.json()
         status = request.status_code
         print(status)
